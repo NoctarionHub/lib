4b2839a1e8309ee603060c12ae65127ae6e25443
NoctarionHub <izzibro29@gmail.com>
2026-09-22 02:47:17 +0700

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -15462,19 +15462,9 @@ function Window.new(properties)
         Parent = self.logoFrame,
     })
 
-    -- shade gelap biar teks kebaca
-    self.logoShade = self:Create("Frame", {
-        Size = UDim2.new(1, 0, 1, 0),
-        BackgroundColor3 = Color3.fromRGB(0, 0, 0),
-        BackgroundTransparency = 1,
-        BorderSizePixel = 0,
-        ZIndex = 1,
-        Parent = self.logoFrame,
-    })
-
     self:Create("UICorner", {
         CornerRadius = UDim.new(0, 12),
-        Parent = self.logoShade,
+        Parent = self.logoFrame,
     })
 
     self:Create("UIGradient", {
