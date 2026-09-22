d8d246ccaa490b51791fd45a001e9d3939bca194
NoctarionHub <izzibro29@gmail.com>
2026-09-22 22:11:13 +0700

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -15440,7 +15440,7 @@ function Window.new(properties)
         self.logoTitleLabel = self:Create("TextLabel", {
             Text = self.logoTitle,
             Size = UDim2.new(1, 0, 0, 20),
-            Position = UDim2.new(0.5, 0, 0.5, 0),
+            Position = UDim2.new(0.5, 0, 1, 0),
             AnchorPoint = Vector2.new(0.5, 0.5),
             BackgroundTransparency = 1,
             TextSize = 18,
