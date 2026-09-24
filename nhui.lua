c84ae1cbd928756a534800765be5a303167eeb4a
NoctarionHub <izzibro29@gmail.com>
2026-09-25 00:02:32 +0700

    Update nhui.lua

--- nhui.lua
+++ nhui.lua
@@ -9336,11 +9336,10 @@ function Tab:AddInfoGrid(opts)
 	local GRID_GAP = 8
 	local rows = math.ceil(#items / columns)
 	local gridH = rows > 0 and (rows * CHIP_H + (rows - 1) * GRID_GAP) or 0
-		local BUTTON_H, BUTTON_GAP = 32, 8
+	local BUTTON_H, BUTTON_GAP = 32, 8
 	local hasFooter = (opts.ButtonText and opts.ButtonText ~= "") or (opts.Button2Text and opts.Button2Text ~= "")
 	local buttonBlockH = hasFooter and (BUTTON_GAP + BUTTON_H) or 0
-		local height = PAD * 2 + HEADER_H + (rows > 0 and (10 + gridH) or 0) + buttonBlockH + (hasFooter and 6 or 0)
- 
+	local height = PAD * 2 + HEADER_H + (rows > 0 and (10 + gridH) or 0) + buttonBlockH + (hasFooter and 6 or 0) 
 	local card = BaseCard(self._page, height)
 	self._window:_RegisterSearchable(self, title, card)
  
